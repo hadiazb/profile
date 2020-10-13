09997bd7e692a26cc821e1728eb2f46f092341e9
hadiazb <hadiazb@unal.edu.co>
2020-10-12 21:06:16 -0500

    #20 adding post cards

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,13 +1,175 @@
 import React from 'react';
 import './style.scss';
+import { SiNotion, SiYoutube } from 'react-icons/si';
 
 const Blog = () => {
 	return (
 		<div className='blog'>
 			<h3>Blog</h3>
-			<h5>
-				Estamos trabajando para actualizar estas secciones
-			</h5>
+			<div className='blog__header'>
+				<ul className='blog__header-items'>
+					<li>
+						<div className='box-left'>
+							<SiNotion size='13' />
+						</div>
+						<div className='box-center'></div>
+						<div className='box-right'>
+							<h6>
+								¡Como conseguir tu primer empleo como programador!
+							</h6>
+							<p>
+								Basado en mi experiencia y resumido en 10 puntos, te
+								explico lo que necesitas saber para conseguir tu
+								primer empleo como programador.
+							</p>
+						</div>
+					</li>
+					<li>
+						<div className='box-left'>
+							<SiYoutube size='13' />
+						</div>
+						<div className='box-center'></div>
+						<div className='box-right'>
+							<h6>
+								¡Como conseguir tu primer empleo como programador!
+							</h6>
+							<p>
+								Basado en mi experiencia y resumido en 10 puntos, te
+								explico lo que necesitas saber para conseguir tu
+								primer empleo como programador.
+							</p>
+						</div>
+					</li>
+					<li>
+						<div className='box-left'>
+							<SiNotion size='13' />
+						</div>
+						<div className='box-center'></div>
+						<div className='box-right'>
+							<h6>
+								¡Como conseguir tu primer empleo como programador!
+							</h6>
+							<p>
+								Basado en mi experiencia y resumido en 10 puntos, te
+								explico lo que necesitas saber para conseguir tu
+								primer empleo como programador.
+							</p>
+						</div>
+					</li>
+					<li>
+						<div className='box-left'>
+							<SiNotion size='13' />
+						</div>
+						<div className='box-center'></div>
+						<div className='box-right'>
+							<h6>
+								¡Como conseguir tu primer empleo como programador!
+							</h6>
+							<p>
+								Basado en mi experiencia y resumido en 10 puntos, te
+								explico lo que necesitas saber para conseguir tu
+								primer empleo como programador.
+							</p>
+						</div>
+					</li>
+					<li>
+						<div className='box-left'>
+							<SiNotion size='13' />
+						</div>
+						<div className='box-center'></div>
+						<div className='box-right'>
+							<h6>
+								¡Como conseguir tu primer empleo como programador!
+							</h6>
+							<p>
+								Basado en mi experiencia y resumido en 10 puntos, te
+								explico lo que necesitas saber para conseguir tu
+								primer empleo como programador.
+							</p>
+						</div>
+					</li>
+					<li>
+						<div className='box-left'>
+							<SiNotion size='13' />
+						</div>
+						<div className='box-center'></div>
+						<div className='box-right'>
+							<h6>
+								¡Como conseguir tu primer empleo como programador!
+							</h6>
+							<p>
+								Basado en mi experiencia y resumido en 10 puntos, te
+								explico lo que necesitas saber para conseguir tu
+								primer empleo como programador.
+							</p>
+						</div>
+					</li>
+					<li>
+						<div className='box-left'>
+							<SiNotion size='13' />
+						</div>
+						<div className='box-center'></div>
+						<div className='box-right'>
+							<h6>
+								¡Como conseguir tu primer empleo como programador!
+							</h6>
+							<p>
+								Basado en mi experiencia y resumido en 10 puntos, te
+								explico lo que necesitas saber para conseguir tu
+								primer empleo como programador.
+							</p>
+						</div>
+					</li>
+					<li>
+						<div className='box-left'>
+							<SiNotion size='13' />
+						</div>
+						<div className='box-center'></div>
+						<div className='box-right'>
+							<h6>
+								¡Como conseguir tu primer empleo como programador!
+							</h6>
+							<p>
+								Basado en mi experiencia y resumido en 10 puntos, te
+								explico lo que necesitas saber para conseguir tu
+								primer empleo como programador.
+							</p>
+						</div>
+					</li>
+					<li>
+						<div className='box-left'>
+							<SiNotion size='13' />
+						</div>
+						<div className='box-center'></div>
+						<div className='box-right'>
+							<h6>
+								¡Como conseguir tu primer empleo como programador!
+							</h6>
+							<p>
+								Basado en mi experiencia y resumido en 10 puntos, te
+								explico lo que necesitas saber para conseguir tu
+								primer empleo como programador.
+							</p>
+						</div>
+					</li>
+					<li>
+						<div className='box-left'>
+							<SiNotion size='13' />
+						</div>
+						<div className='box-center'></div>
+						<div className='box-right'>
+							<h6>
+								¡Como conseguir tu primer empleo como programador!
+							</h6>
+							<p>
+								Basado en mi experiencia y resumido en 10 puntos, te
+								explico lo que necesitas saber para conseguir tu
+								primer empleo como programador.
+							</p>
+						</div>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
